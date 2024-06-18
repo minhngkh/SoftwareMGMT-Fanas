698412bec1f9674bef4336389099d02a4676c06b
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-06-18 15:05:18 +0700

    restore app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const ip = require("ip");
+const { storage, getDownloadURL } = require('./firebase.js');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/get-audio-url', async (req, res) => {
+    console.log(req.query);
+
+    try {
+        const filename = "sw" + req.query.chapters + ".mp3";
+        const fileRef = storage.bucket().file('SnowWhite/' + filename);
+        const url = await getDownloadURL(fileRef);
+        res.json({ url });
+    } catch (error) {
+        console.error("Error fetching the download URL: ", error);
+        res.status(500).send("Error fetching the download URL");
+    }
+});
+
+const PORT = 8080;
+
+app.listen(PORT, ip.address(), () => {
+    console.log(`Server is listening on port http://${ip.address()}:${PORT}`);
+});
