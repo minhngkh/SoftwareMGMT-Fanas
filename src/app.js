f85f0d9f50a159fa1f21b833b7a7d9e236f28bdd
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-07-06 17:12:57 +0700

    add router, refractor code

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const ip = require("ip");
 const handleBars = require('express-handlebars');
+
 require("dotenv").config();
 
-const { storage, getDownloadURL } = require('./config/firebase.js');
-const firebaseAuthController = require('./controllers/firebaseAuthController.js');
+const route = require('./routes/index.js');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
@@ -29,42 +29,7 @@ app.engine('hbs', handleBars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource', 'views'));
 
-app.post('/register', firebaseAuthController.registerUser);
-app.post('/login', firebaseAuthController.loginUser);
-app.post('/logout', firebaseAuthController.logoutUser);
-
-app.get('/', (req, res) => {
-  //res.render('homepage', {layout: 'main'});
-  //res.render('signup', {layout: 'main'});
-  res.render('index', {layout: 'main'});
-});
-
-app.get('/get-audio-url', async (req, res) => {
-    console.log(req.query);
-
-    try {
-        const filename = "sw" + req.query.chapters + ".mp3";
-        const fileRef = storage.bucket().file('SnowWhite/' + filename);
-        const url = await getDownloadURL(fileRef);
-        res.json({ url });
-    } catch (error) {
-        console.error("Error fetching the download URL: ", error);
-        res.status(500).send("Error fetching the download URL");
-    }
-});
-
-app.get('/example', (req, res) => {
-  const exampleText = "muahahahaha";
-  res.render("example", { layout: "example", exampleText});
-});
-
-app.get('/detail', (req, res) => {
-  res.render("detail");
-});
-
-app.get('/search', (req, res) => {
-  res.render("search");
-});
+route(app);
 
 const PORT = 8080;
 
