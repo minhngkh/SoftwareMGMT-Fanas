c37f54df45d8b6e1bcb1074604006eb291f66b6b
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-08-13 20:14:02 +0700

    update playback progress bar handler, update progress bar UI

--- public/js/playback.js
+++ public/js/playback.js
@@ -2,7 +2,12 @@ const PausePlayBtn = document.querySelector('.button-play i')
 
 // code mới được chuyển từ hbs qua
 const audioPlayer = document.getElementById('audioPlayer');
+const durationContainer = document.getElementById('duration');
+const slider = document.getElementById('progress-slider');
+const currentTimeContainer = document.getElementById('current-time');
+// const progressContainer = document.getElementById('progress-container');
 
+let rAF = null;
 
 async function fetchAudioUrl() {
     try {
@@ -18,7 +23,7 @@ async function fetchAudioUrl() {
 }
 
 fetchAudioUrl();
-alert('Press play button to start');
+// alert('Press play button to start');
 
 PausePlayBtn.addEventListener("click", () => {
     //console.log('Button clicked');
@@ -27,6 +32,7 @@ PausePlayBtn.addEventListener("click", () => {
         //console.log('Playing audio');
     } else {
         audioPlayer.pause();
+        cancelAnimationFrame(rAF);
         //console.log('Pausing audio');
     }
     //console.log('Audio paused state:', audioPlayer.paused);
@@ -36,6 +42,7 @@ audioPlayer.addEventListener("play", () => {
     //console.log('Audio started playing');
     PausePlayBtn.classList.remove("fa-play");
     PausePlayBtn.classList.add("fa-pause");
+    requestAnimationFrame(whilePlaying);
 });
 
 audioPlayer.addEventListener("pause", () => {
@@ -43,3 +50,56 @@ audioPlayer.addEventListener("pause", () => {
     PausePlayBtn.classList.add("fa-play");
     //console.log('Audio paused');
 });
+
+slider.addEventListener('input', () => {
+    currentTimeContainer.textContent = calculateTime(slider.value);
+    if(!audioPlayer.paused) {
+        cancelAnimationFrame(rAF);
+    }
+});
+
+slider.addEventListener('change', () => {
+    audioPlayer.currentTime = slider.value;
+    if(!audioPlayer.paused) {
+        requestAnimationFrame(whilePlaying);
+    }
+});
+
+const setSliderMax = () => {
+    slider.max = Math.floor(audioPlayer.duration);
+}
+
+const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+}
+
+const displayDuration = () => {
+    durationContainer.textContent = calculateTime(audioPlayer.duration);
+}
+
+// const displayBufferedAmount = () => {
+//     const bufferedAmount = Math.floor(audioPlayer.buffered.end(audioPlayer.buffered.length - 1));
+//     console.log((bufferedAmount / slider.max) * 100);
+//     progressContainer.style.setProperty('--buffered-width', `${(bufferedAmount / slider.max) * 100}%`);
+// }
+
+const whilePlaying = () => {
+    slider.value = Math.floor(audioPlayer.currentTime);
+    currentTimeContainer.textContent = calculateTime(slider.value);
+    rAF = requestAnimationFrame(whilePlaying);
+}
+
+if (audioPlayer.readyState > 0) {
+    displayDuration();
+    setSliderMax();
+    // displayBufferedAmount();
+  } else {
+    audioPlayer.addEventListener('loadedmetadata', () => {
+        displayDuration();
+        setSliderMax();
+        // displayBufferedAmount();
+    });
+  }
